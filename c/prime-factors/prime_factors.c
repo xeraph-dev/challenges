0765fc4200f5d66f390433403c470497095ca48c
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-02-14 12:35:04 -0500

    feat(c): solve Prime Factors

---
+++ c/prime-factors/prime_factors.c
@@ -0,0 +1,41 @@
+#include "prime_factors.h"
+
+#include <iso646.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static bool is_prime(uint64_t num) {
+    if (num < 2)
+        return false;
+    if (num == 2 or num == 3)
+        return true;
+    if (num % 2 == 0 or num % 3 == 0)
+        return false;
+
+    for (uint64_t i = 5; i * i <= num; i += 2) {
+        if (num % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+size_t find_factors(uint64_t n, uint64_t factors[static MAXFACTORS]) {
+    size_t factor_len = {0};
+    uint64_t prime = {2};
+
+    while (n > 1) {
+        while (n % prime == 0) {
+            factors[factor_len++] = prime;
+            n /= prime;
+        }
+        prime = (prime == 2) ? 3 : prime + 2;
+        while (not is_prime(prime))
+            prime += 2;
+    }
+
+    return factor_len;
+}
