d39b45329fd4d788d673adeaa1abe9dda6625d9d
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-06-23 19:08:58 -0400

    [cis-194] feat(01-intro): solve exercise 5

--- cis-194/01-intro.hs
+++ cis-194/01-intro.hs
@@ -20,3 +20,11 @@ sumDigits = sum . map (sum . toDigits)
 
 validate :: Integer -> Bool
 validate = (== 0) . (`mod` 10) . sumDigits . doubleEveryOther . toDigits
+
+type Peg = String
+
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
