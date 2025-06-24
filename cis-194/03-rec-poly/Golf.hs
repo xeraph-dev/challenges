bb3f90567568501c76f21b375db9067d35ba04f6
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-06-24 19:23:36 -0400

    [cis-194] feat(03-rec-poly): solve exercise 2

--- cis-194/03-rec-poly/Golf.hs
+++ cis-194/03-rec-poly/Golf.hs
@@ -5,3 +5,9 @@ skips xs = map (map snd) $ map f $ zip [1 ..] $ replicate (length xs) $ zip [1 .
   where
     f (idx, sxs) = filter (ff idx) sxs
     ff idx x = fst x `mod` idx == 0
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = map (!! 1) $ filter f $ map fm $ zip [-1 ..] $ replicate (length xs) xs
+  where
+    f xs = length xs == 3 && maximum xs == xs !! 1
+    fm (idx, xs) = take (min 3 $ idx + 3) $ drop idx xs
