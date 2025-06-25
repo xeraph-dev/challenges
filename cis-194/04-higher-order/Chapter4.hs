77435424bad023c5e1da40399b6e378652486690
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-06-25 14:23:03 -0400

    [cis-194] feat(04-higher-order): solve exercise 1

---
+++ cis-194/04-higher-order/Chapter4.hs
@@ -0,0 +1,11 @@
+module Chapter4 where
+
+fun1 :: [Integer] -> Integer
+fun1 = product . map (+ (-2)) . filter even
+
+fun2 :: Integer -> Integer
+fun2 = sum . filter even . takeWhile (> 1) . iterate f
+  where
+    f n
+      | even n = n `div` 2
+      | otherwise = 3 * n + 1
