307cb544685921459c0a1c8168bdc182e0143205
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-06-24 20:24:49 -0400

    [cis-194] feat(03-rec-poly): solve exercise 3

--- cis-194/03-rec-poly/Golf.hs
+++ cis-194/03-rec-poly/Golf.hs
@@ -1,5 +1,7 @@
 module Gold where
 
+import Data.List
+
 skips :: [a] -> [[a]]
 skips xs = map (map snd) $ map f $ zip [1 ..] $ replicate (length xs) $ zip [1 ..] xs
   where
@@ -11,3 +13,16 @@ localMaxima xs = map (!! 1) $ filter f $ map fm $ zip [-1 ..] $ replicate (lengt
   where
     f xs = length xs == 3 && maximum xs == xs !! 1
     fm (idx, xs) = take (min 3 $ idx + 3) $ drop idx xs
+
+histogram :: [Integer] -> String
+histogram xs = unlines $ (map f $ zip (reverse [1 .. lvls]) $ replicate lvls [0 .. 9]) ++ ["=========="] ++ ["0123456789"]
+  where
+    frep xs = (length xs, head xs)
+    rep = map frep $ group $ sort xs
+
+    lvls = maximum $ map fst rep
+
+    f (lvl, xs) = map (ff lvl) xs
+    ff lvl idx = case find (\x -> snd x == idx) rep of
+      Just (c, _) -> if lvl <= c then '*' else ' '
+      Nothing -> ' '
