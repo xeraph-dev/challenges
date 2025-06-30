4dbf7331f9d455cd518a6eae625eb5fa152f59bc
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2025-06-29 23:04:38 -0400

    [cis-194] feat(04-higher-order): solve exercise 2

--- cis-194/04-higher-order/Chapter4.hs
+++ cis-194/04-higher-order/Chapter4.hs
@@ -9,3 +9,21 @@ fun2 = sum . filter even . takeWhile (> 1) . iterate f
     f n
       | even n = n `div` 2
       | otherwise = 3 * n + 1
+
+data Tree a
+  = Leaf
+  | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree [] = Leaf
+foldTree [a] = Node 0 Leaf a Leaf
+foldTree xs = Node h left a right
+  where
+    mid = length xs `div` 2
+    (leftxs, a : rightxs) = splitAt mid xs
+    (left, right) = (foldTree leftxs, foldTree rightxs)
+    h = 1 + max (height left) (height right)
+
+    height Leaf = 0
+    height (Node h _ _ _) = h
